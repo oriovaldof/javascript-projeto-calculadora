54b88edc6c2cd60693d957ebaf7a766bebdc904d
Oriovaldo de Jesus Fialho <oriovaldof@gmail.com>
2020-04-21 18:11:27 -0400

    limitando 10 caracter visor

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -388,6 +388,12 @@ class CalcController {
     }
 
     set displayCalc(value) {
+
+        if(value.toString().length > 10){
+            this.setError();
+            return false;
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
